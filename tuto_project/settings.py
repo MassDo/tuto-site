b404f8f974b85d1324b991f8234c798fc1bf296f
massdo <dorian_massoulier@hotmail.com>
2020-10-23 21:25:22 +0200

    modif setting for deploy

--- tuto_project/settings.py
+++ tuto_project/settings.py
@@ -18,14 +18,20 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    SECRET_KEY= os.environ['DJANGO_SECRET_KEY']
+    DEBUG = False
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%xio@d&7se0@e!3!v9ghrt#dt$2g_ayy7441b_z(#k9aha7bu9'
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'ko$8hp%c$uyb7h1tcq*@8_+l(d+0bls5-#z(9bjhaf7r5%4!li'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
